ad87145bb28cf98cb91f477e45eee301de7e3220
Сурков Павел Алексеевич <pavel.surkov@bscmsc.ru>
2024-07-06 16:19:36 +0300

    first commit

---
+++ src/main/java/ru/surpavel/irisocserver/IrbisOcrServerApplication.java
@@ -0,0 +1,13 @@
+package ru.surpavel.irisocserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrbisOcrServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrbisOcrServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/surpavel/irisocserver/IrbisOcrServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.surpavel.irisocserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrbisOcrServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
